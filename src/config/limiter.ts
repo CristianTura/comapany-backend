58faa2f9622428e68061e6e1d2da853cf6621b37
CristianB <cristianbuenaventura15@gmail.com>
2025-05-29 11:20:50 -0500

    fix limit

--- src/config/limiter.ts
+++ src/config/limiter.ts
@@ -3,6 +3,6 @@ import { rateLimit } from "express-rate-limit"
 export const limiter =
     rateLimit({
         windowMs: 60 * 1000,
-        limit: process.env.NODE_ENV == 'production' ? 5 : 100,
+        limit: process.env.NODE_ENV == 'production' ? 100 : 120,
         message: {"error" : 'Demasiadas peticiones, intenta luego.'}
     })
